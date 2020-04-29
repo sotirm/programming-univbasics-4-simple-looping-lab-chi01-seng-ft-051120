3b02e690b95601ac612dd3629774d60f7023cdf5
Michael Sotir <sotirm@umich.edu>
2020-04-29 21:13:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -32,9 +32,10 @@ def return_string_array(a_arr)
   counter = 0
   new_arr = []
   while counter <= a_arr.length do 
-    new_arr.push a_arr[counter].
-  
+    new_arr.push a_arr[counter].to_s
+    counter = counter + 1
   end
+  new_arr
 end
 
 
